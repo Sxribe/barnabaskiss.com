c1819dff34b5e29c250b022605acc4f1b65ca826
Barnabas Kiss <barnabas.05.kiss@gmail.com>
2021-02-27 14:22:45 -0500

    Update <Nav />'s tests

--- components/nav/nav.spec.tsx
+++ components/nav/nav.spec.tsx
@@ -4,26 +4,36 @@ import Nav from "./nav";
 
 describe("<Nav />", () => {
   it("should render light logo", () => {
-    const nav = shallow(<Nav logoStyle="light" hamburgerSyle="light" />);
+    const nav = shallow(<Nav logoStyle="light" hamburgerSyle="light" hamburgerPressed={() => null} />);
     let correctImages = nav.findWhere(el => el.prop("src") === "/logo-light.svg");
     expect(correctImages).toHaveLength(1);
   })
 
   it("should render dark logo", () => {
-    const nav = shallow(<Nav logoStyle="dark" hamburgerSyle="light" />);
+    const nav = shallow(<Nav logoStyle="dark" hamburgerSyle="light" hamburgerPressed={() => null} />);
     let correctImages = nav.findWhere(el => el.prop("src") === "/logo-dark.svg");
     expect(correctImages).toHaveLength(1);
   })
   
   it("should render light hamburger", () => {
-    const nav = shallow(<Nav logoStyle="light" hamburgerSyle="light" />);
+    const nav = shallow(<Nav logoStyle="light" hamburgerSyle="light" hamburgerPressed={() => null} />);
     let correctImages = nav.findWhere(el => el.prop("src") === "/assets/hamburger-light.svg");
     expect(correctImages).toHaveLength(1);
   })
   
   it("should render dark hamburger", () => {
-    const nav = shallow(<Nav logoStyle="light" hamburgerSyle="dark" />);
+    const nav = shallow(<Nav logoStyle="light" hamburgerSyle="dark" hamburgerPressed={() => null} />);
     let correctImages = nav.findWhere(el => el.prop("src") === "/assets/hamburger-dark.svg");
     expect(correctImages).toHaveLength(1);
+  })
+
+  it("should detect the hamburger being pressed", () => {
+    let pressed = false;
+    let changePressed = () => pressed = true;
+    const nav = shallow(<Nav logoStyle="light" hamburgerSyle="light" hamburgerPressed={changePressed} />);
+    
+    let element = nav.findWhere(el => el.prop("alt") === "Menu")
+    element.simulate("click");
+    expect(pressed).toBe(true);
   })
 })
