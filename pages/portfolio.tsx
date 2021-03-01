9164240ad221cdf4df8fd55ca2fe6f4d93c9caed
Barnabas Kiss <barnabas.05.kiss@gmail.com>
2021-03-01 14:08:08 -0500

    Add portfolio under construction page

---
+++ pages/portfolio.tsx
@@ -0,0 +1,57 @@
+import { useState, useRef, useEffect } from "react";
+import Head from "next/head";
+import Nav from "../components/nav/nav";
+import styles from "../styles/index.module.scss";
+import Concealer from "../components/concealer/concealer";
+import NavCover from "../components/navcover/navcover";
+import { useRouter } from "next/router";
+
+export default function Home() {
+  const router = useRouter();
+
+  const [transitioning, setTransitioning] = useState<boolean>(typeof router.query.transitioning !== undefined);
+  const [sliderOpen, setSliderOpen] = useState<boolean>(transitioning);
+  const [navCoverOpen, setNavCoverOpen] = useState<boolean>(false);
+  const [logoColor, setLogoColor] = useState<"light"|"dark">(transitioning ? "light" : "dark");
+  const sidebarRef = useRef<HTMLImageElement>(null!)
+
+  useEffect(() => {
+    if (transitioning === false) {
+      if (sliderOpen) {
+        setTimeout(() => setLogoColor("light"), .3 * 1000);
+        setTimeout(() => setNavCoverOpen(true), .4 * 1000);
+      } else {
+        setLogoColor("dark")
+        setNavCoverOpen(false);
+      }
+    } else {
+      setSliderOpen(false);
+      setTimeout(() => {
+        setTransitioning(false);
+        setLogoColor("dark");
+        router.push("/portfolio");
+      }, .4 * 1000);
+    }
+  }, [sliderOpen])
+  
+  return (
+    <>
+      <Head>
+        <title>Barnabás Kiss</title>
+      </Head>
+      
+      <Nav logoStyle={logoColor} hamburgerSyle="light" hamburgerPressed={() => setSliderOpen(!sliderOpen)}/>
+      <NavCover open={navCoverOpen} />
+      <main className={styles.main}>
+        <div className={styles.content}>
+          <h1 style={{marginBottom: 0}}><span>This page is currently construction</span></h1>
+          <h2>Please check back soon.</h2>
+          <div className={styles.buttons}>
+          </div>
+        </div>
+        <img src="/assets/sidebar.svg" alt="" ref={sidebarRef} />
+        <Concealer open={sliderOpen || transitioning} sidebarRef={sidebarRef} startClosed={true} />
+      </main>
+    </>
+  )
+}
